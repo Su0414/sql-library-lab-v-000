52941a229dcac49e1226f35e1bd9148ba0bb7354
Suchitra Naik <susanaik@gmail.com>
2018-05-17 20:21:36 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -5,11 +5,13 @@ end
 
 def select_name_and_motto_of_char_with_longest_motto
   "Write your SQL query here"
+  "select name, motto from characters order by motto"
 end
 
 
 def select_value_and_count_of_most_prolific_species
   "Write your SQL query here"
+
 end
 
 def select_name_and_series_subgenres_of_authors
