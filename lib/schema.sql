5799667da10c4a771a182ae1758ec1af16fe05cb
Suchitra Naik <susanaik@gmail.com>
2018-05-17 18:32:31 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -4,3 +4,21 @@ CREATE TABLE series(
   subgenre_id INTEGER,
   id INTEGER PRIMARY KEY
 );
+
+
+CREATE TABLE subgenres(
+  name TEXT,
+  id INTEGER PRIMARY KEY
+);
+
+CREATE TABLE authors(
+  name TEXT,
+  id INTEGER PRIMARY KEY
+);
+
+CREATE TABLE books(
+  title TEXT,
+  year TEXT,
+  series_id INTEGER,
+  id INTEGER PRIMARY KEY
+);
